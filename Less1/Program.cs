4c7a427e0adf4e9055a7bd6aa81459c4552adcf7
Nilahim <ejik73rus5@mail.com>
2023-01-06 02:59:27 +0400

    Первая прога с приветом

--- Less1/Program.cs
+++ Less1/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World and Nilahim!");
+Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("В красном углу, ");
+Console.WriteLine(username);
+
 // https://github.com/Nilahim73/C-learn.git
