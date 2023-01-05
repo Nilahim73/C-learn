bbbcfcc7dbb8de1a2ed767387f58bacdd216b3b4
Nilahim <ejik73rus5@mail.com>
2023-01-06 03:08:43 +0400

    Проверка

--- Less1/Program.cs
+++ Less1/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Введите ваше имя: ");
 string username = Console.ReadLine();
 Console.Write("В красном углу, ");
 Console.WriteLine(username);
 
-// https://github.com/Nilahim73/C-learn.git
+// https://github.com/Nilahim73/C-learn.gitз
