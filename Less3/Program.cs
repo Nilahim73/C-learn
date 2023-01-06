f4af0f761969c12eed16b71df568277e2779938a
Nilahim <ejik73rus5@mail.com>
2023-01-07 00:12:22 +0400

    Деление

--- Less3/Program.cs
+++ Less3/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello Nilahim and World!");
+﻿double numbA = 14;
+double numbb = 5;
+Console.WriteLine(numbA / numbb);
