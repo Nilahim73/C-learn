fbb92014b81615344d0bc2e0b114344476e281ec
Nilahim <ejik73rus5@mail.com>
2023-01-09 01:02:17 +0400

    Максимум трех

---
+++ Dom1seminar/zadacha2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = Math.Max(num1, Math.Max(num2,num3));
+Console.WriteLine("Наибольшее число: " + max);
