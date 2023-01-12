f3ede4602fb230c1f1647065bc4eb4cebe009315
Nilahim <ejik73rus5@mail.com>
2023-01-12 23:19:17 +0400

    2-я цифра трехзначного числа

---
+++ DZ2/Z10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введи трёхзначное число: ");
+
+int numb = Convert.ToInt32(Console.ReadLine());
+string stringNumb = Convert.ToString(numb);
+
+Console.WriteLine("Вторая цифра числа >?> "+stringNumb[1]);
