df259c440797c2731ad5df0327e28a873d478699
Nilahim <ejik73rus5@mail.com>
2023-01-13 00:33:46 +0400

    Определение выходного дня

---
+++ DZ2/Z15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumb = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumb) 
+{
+  if (dayNumb == 6 || dayNumb == 7) 
+  {
+  Console.WriteLine("Да, выходной");
+  }
+  else if (dayNumb < 1 || dayNumb > 7) 
+  {
+    Console.WriteLine("!Введена несоответствующая цифра!");
+  }
+  else Console.WriteLine("Рабочий день");
+}
+
+CheckingTheDayOfTheWeek(dayNumb);
