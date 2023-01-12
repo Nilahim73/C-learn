e78a66a2df78a5c7c586d3b0a1316585f5058a6c
Nilahim <ejik73rus5@mail.com>
2023-01-12 23:41:20 +0400

    3-я цифра заданного числа

---
+++ DZ2/Z13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+
+int numb = Convert.ToInt32(Console.ReadLine());
+string stringNumb = Convert.ToString(numb);
+
+if (stringNumb.Length > 2)
+{
+  Console.WriteLine("Третья цифра : " + stringNumb[2]);
+}
+else {
+  Console.WriteLine("!Третьей цифры нет!");
+}
