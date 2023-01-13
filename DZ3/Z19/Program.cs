7019d67123f610fd6badd2e7d93cbd560a0fc4c9
Nilahim <ejik73rus5@mail.com>
2023-01-13 23:09:11 +0400

    Папалиндромом

---
+++ DZ3/Z19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Палиндромом называют любой симметричный относительно своей середины набор символов.");
+Console.Write("Введите 5-ти значное число: ");
+string numb = Console.ReadLine()!;
+
+void CheckingNumber(string numb)
+{
+  if (numb[0]==numb[4] || numb[1]==numb[3])
+  {
+    Console.WriteLine($"Число: {numb} - палиндром.");
+  }
+  else Console.WriteLine($"Число: {numb} - Не палиндром.");
+}
+
+if (numb!.Length == 5)
+{
+  CheckingNumber(numb);
+}
+else Console.WriteLine($"Введите правильное число");
