3a0f5d7bc82de71505a0f03a838aca44fc538543
Nilahim <ejik73rus5@mail.com>
2023-01-17 16:08:39 +0400

    3х значные массивы и чётность заполнения

---
+++ DZ5/Z34/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int count = 0;
+ExaminatuonArray(numbers);
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(99, 1000);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void ExaminatuonArray(int[] numbers)
+{
+    for (int z = 0; z < numbers.Length; z++)
+        if (numbers[z] % 2 == 0)
+            count++;
+
+    Console.WriteLine($"Количество чисел: {numbers.Length}, из них {count} - чётные.");
+}
