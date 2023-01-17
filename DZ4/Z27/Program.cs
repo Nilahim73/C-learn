846e74f025c5100bedee17ee9cac370e0ae14170
Nilahim <ejik73rus5@mail.com>
2023-01-17 13:22:07 +0400

    Cумма цифр числа

---
+++ DZ4/Z27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (number > 0)
+{
+int num = number % 10;
+number = number / 10;
+sum = sum + num;
+}
+Console.WriteLine("Сумма всех цифр в числе равна: " + sum);
