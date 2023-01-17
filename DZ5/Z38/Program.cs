ec3695e735b0cc1c29bc485b57097197ac470a0d
Nilahim <ejik73rus5@mail.com>
2023-01-17 17:20:45 +0400

    Массив и разница максимального и минимального значения

---
+++ DZ5/Z38/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+MinMaxElementArray(numbers);
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
+    }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void MinMaxElementArray(double[] numbers)
+{
+    for (int z = 0; z < numbers.Length; z++)
+    {
+        if (numbers[z] > max)
+        {
+            max = numbers[z];
+        }
+        if (numbers[z] < min)
+        {
+            min = numbers[z];
+        }
+    }
+}
+Console.WriteLine($"Колличество чисел: {numbers.Length} \nМаксимальное значение = {max} \nМинимальное значение = {min}");
+Console.WriteLine($"Разница значений = {max - min}");
+
