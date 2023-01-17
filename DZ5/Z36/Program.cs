efc5d3a8eed51339206c593bec3e866bbb12867b
Nilahim <ejik73rus5@mail.com>
2023-01-17 16:48:14 +0400

    Массив и сумма чисел на нечетной позиции

---
+++ DZ5/Z36/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int sum = 0;
+ExaminationArray(numbers);
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-10, 10);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void ExaminationArray(int[] numbers)
+{
+    for (int z = 0; z < numbers.Length; z += 2)
+        sum = sum + numbers[z];
+
+    Console.WriteLine($"Количество чисел: {numbers.Length}, сумма элементов на нечётных позициях = {sum}");
+}
